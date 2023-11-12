b9f9517924b894caf9346986e60e273153bdfdd3
ahmdpt101 <ahmdpt101@gmail.com>
2023-11-12 22:30:26 +0300

    Clothes Products Section

--- lib/models/ClothesModel.dart
+++ lib/models/ClothesModel.dart
@@ -5,12 +5,20 @@ import 'package:flutter/material.dart';
 class ClothesModel {
   String photo;
   String title;
+  String category;
+  int price;
+  int oldPrice;
+  int currency;
   int rate;
 
   ClothesModel({
     required this.photo,
     required this.title,
     required this.rate,
+    required this.category,
+    required this.currency,
+    required this.price,
+    required this.oldPrice,
   });
 
   static List<ClothesModel> getClothes() {
@@ -19,36 +27,52 @@ class ClothesModel {
 // Shoes Category
     clothes.add(
       ClothesModel(
-        photo: 'assets/categories_photos/shoes.png',
-        title: 'SHOES',
+        photo: 'assets/categories_photos/product.png',
+        title: 'Sparkle Blossom Dress',
+        category: 'T-SHIRT',
         rate: 4,
+        currency: 4,
+        price: 45,
+        oldPrice: 55,
       ),
     );
 
 // Accesories Category
     clothes.add(
       ClothesModel(
-        photo: 'assets/categories_photos/shoes.png',
-        title: 'SHOES',
-        rate: 4,
+        photo: 'assets/categories_photos/product2.png',
+        title: 'Starlight Elegance Gown',
+        category: 'DRESS',
+        rate: 5,
+        currency: 3,
+        price: 45,
+        oldPrice: 55,
       ),
     );
 
 // Clothes Category
     clothes.add(
       ClothesModel(
-        photo: 'assets/categories_photos/shoes.png',
-        title: 'SHOES',
-        rate: 4,
+        photo: 'assets/categories_photos/product3.png',
+        title: 'Whimsical Flutter Skirt',
+        category: 'SHOE',
+        rate: 2,
+        currency: 2,
+        price: 45,
+        oldPrice: 95,
       ),
     );
 
 // Extra category
     clothes.add(
       ClothesModel(
-        photo: 'assets/categories_photos/shoes.png',
-        title: 'SHOES',
-        rate: 4,
+        photo: 'assets/categories_photos/product.png',
+        title: 'Radiant Rainbow Tutu',
+        category: 'ACCESSORIES',
+        rate: 1,
+        currency: 1,
+        price: 45,
+        oldPrice: 75,
       ),
     );
 
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -27,55 +27,227 @@ class HomePage extends StatelessWidget {
           SizedBox(
             height: 15,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Text(
-                  "Categories",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: "Lato",
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Container(
-                height: 90,
-                child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                    padding: EdgeInsets.only(left: 20, right: 20),
-                    itemCount: categories.length,
-                    separatorBuilder: (context, index) => SizedBox(
-                          width: 20,
-                        ),
-                    itemBuilder: (context, index) {
-                      return Container(
-                        height: 50,
-                        width: 120,
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                              color: categories[index].borderColor,
-                              width: 1.0,
-                            ),
-                            image: DecorationImage(
-                              image: AssetImage(categories[index]
-                                  .photo), // Replace with your image asset
-                              fit: BoxFit.cover, // Adjust the BoxFit as needed
-                            ),
-                            borderRadius: BorderRadius.circular(10)),
-                      );
-                    }),
-              )
-            ],
+          categoriesSection(),
+          SizedBox(
+            height: 15,
+          ),
+          clothesSection(),
+          SizedBox(
+            height: 15,
           ),
         ],
       ),
+    );
+  }
+
+  Column clothesSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: Text(
+            'Clothes Products',
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: "Lato",
+                fontWeight: FontWeight.bold,
+                fontSize: 25),
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+            width: 400,
+            height: 400,
+            child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 300,
+                    width: 200,
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF2F8FF),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Stack(
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                // Navigator.push(
+                                //   context,
+                                //   MaterialPageRoute(
+                                //     builder: (context) => AppointScreen(),
+                                //   ),
+                                // );
+                              },
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    topRight: Radius.circular(10)),
+                                child: Image.asset(
+                                  clothes[index].photo,
+                                  height: 200,
+                                  width: 200,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Container(
+                                margin: EdgeInsets.all(8),
+                                width: 45,
+                                height: 25,
+                                decoration: BoxDecoration(
+                                    color: Color.fromARGB(255, 239, 66, 66),
+                                    borderRadius: BorderRadius.circular(20)),
+                                child: Center(
+                                  child: Text(
+                                    "-20%",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: 150.0),
+                                child: Container(
+                                  margin: EdgeInsets.all(8),
+                                  width: 45,
+                                  height: 45,
+                                  decoration: BoxDecoration(
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    shape: BoxShape.circle,
+                                  ),
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.favorite_outline,
+                                      color: Color.fromARGB(255, 180, 180, 180),
+                                      size: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: List.generate(
+                                  clothes[index].rate,
+                                  (starIndex) => Icon(
+                                    Icons.star,
+                                    color: Colors.amber,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                clothes[index].title,
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                    color: const Color.fromARGB(
+                                        255, 157, 156, 156)),
+                              ),
+                              Text(
+                                clothes[index].category,
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black.withOpacity(0.6)),
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                clothes[index].oldPrice.toString(),
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 84, 77, 77),
+                                    decoration: TextDecoration.lineThrough,
+                                    fontSize: 20),
+                              ),
+                              Text(
+                                clothes[index].price.toString(),
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 25,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => SizedBox(
+                      height: 1,
+                    ),
+                itemCount: clothes.length))
+      ],
+    );
+  }
+
+  Column categoriesSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(
+            "Categories",
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: "Lato",
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+          height: 90,
+          child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.only(left: 20, right: 20),
+              itemCount: categories.length,
+              separatorBuilder: (context, index) => SizedBox(
+                    width: 20,
+                  ),
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 50,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: categories[index].borderColor,
+                        width: 1.0,
+                      ),
+                      image: DecorationImage(
+                        image: AssetImage(categories[index]
+                            .photo), // Replace with your image asset
+                        fit: BoxFit.cover, // Adjust the BoxFit as needed
+                      ),
+                      borderRadius: BorderRadius.circular(10)),
+                );
+              }),
+        )
+      ],
     );
   }
 
